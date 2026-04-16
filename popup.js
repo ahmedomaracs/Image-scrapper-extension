fe49a0d88a69a4623da8f3ed41bb9850733382cb
Ahmed Omara <42022132@hti.edu.eg>
2026-04-16 10:51:22 +0200

    feat: improve UI/UX and resolve broken image display issue

--- popup.js
+++ popup.js
@@ -1,28 +1,48 @@
 document.getElementById("getImages").addEventListener("click", async () => {
 
-  const [tab] = await chrome.tabs.query({
-    active: true,
-    currentWindow: true
-  });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: () => {
-      const images = document.querySelectorAll("img");
-      return [...images].map(img => img.src);
-    }
-  }, (results) => {
-
-    const container = document.getElementById("images");
-    container.innerHTML = "";
-
-    results[0].result.forEach(src => {
-      const img = document.createElement("img");
-      img.src = src;
-      img.width = 100;
-      container.appendChild(img);
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
     });
 
-  });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            const images = document.querySelectorAll("img");
+            return [...images].map(img => img.src);
+        }
+    }, (results) => {
+
+        const container = document.getElementById("images");
+        container.innerHTML = "";
+        //images count
+        const images = results[0].result;
+        const count = images.length;
+        document.getElementById("imageCount").textContent = count;
+
+
+        // show/hide empty state
+        if (count === 0) {
+            emptyState.style.display = "block";
+        } else {
+            emptyState.style.display = "none";
+        }
+
+         results[0].result.filter(src => {
+            return src && src.startsWith("http");
+        });
+        results[0].result.forEach(src => {
+            const img = document.createElement("img");
+            img.src = src;
+            img.width = 100;
+            //remove broken images
+            img.onerror = () => {
+                img.remove();
+            };
+
+            container.appendChild(img);
+        });
+
+    });
 
 });
